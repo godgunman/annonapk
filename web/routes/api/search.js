3206c51bf58375de201f35a70303d5a9a65d83f0
dm4 <sunrisedm4@gmail.com>
2013-11-02 11:09:04 +0000

    finish /api/search

--- web/routes/api/search.js
+++ web/routes/api/search.js
@@ -2,12 +2,14 @@ exports.index = function(req, res) {
     var query = req.query.q;
     if (query === undefined || query.match(/^[1-9a-zA-Z ]+$/) == null) {
         res.send({"error": "Invalid query string."});
+        return;
     }
 
     var exec = require('child_process').exec;
     function puts(error, stdout, stderr) {
-        stdout = "<pre>" + stdout + "</pre>";
-        res.send(stdout);
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.write(stdout);
+        res.end();
     }
-    exec("cowsay '" + query + "'", puts);
+    exec("../download_apk/search/search.py '" + query + "'", puts);
 };
