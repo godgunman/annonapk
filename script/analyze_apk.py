acc4fda1d626d9a277f869fde20c199a3b1e3ecf
Hung-Chi Su <atdog.cs96@gmail.com>
2013-11-02 12:39:02 +0000

    return apk_information in json output

--- script/analyze_apk.py
+++ script/analyze_apk.py
@@ -2,7 +2,7 @@
 
 from androguard.decompiler.dad import decompile
 from dm4 import read_apk
-import sys, hashlib, os, errno
+import sys, hashlib, os, errno, json
 
 # Global variables
 base_dir = os.path.dirname(__file__)
@@ -56,11 +56,26 @@ def androguardAnalyze(f_name, f_md5):
             with open(src_name, "ab") as f:
                 f.write("// " + method.get_class_name() + "->" + method.get_name() + method.get_descriptor() + "\n")
                 f.write(decompileMethod(dx, method))
+    return getAPKInformationJson(a,d,dx)
 
 def apktoolAnalyze(f_name, f_md5):
     from subprocess import call
     dir_name = APK_ROOT + "/analytics/" + f_md5 + "/"
     call(["/home/atdog/jdk1.7.0_45/bin/java", "-jar", base_dir + "/apktool.jar", "d", "-f", f_name, dir_name])
+
+def getAPKInformationJson(a, d, dx):
+    result = {}
+    result['package'] = a.get_package()
+    result['class_count'] = len(d.get_classes())
+    result['method_count'] = len(d.get_methods())
+    result['permissions'] = a.get_permissions()
+    result['activities'] = a.get_activities()
+    result['providers'] = a.get_providers()
+    result['services'] = a.get_services()
+    result['receivers'] = a.get_receivers()
+    result['max_sdk_version'] = a.get_max_sdk_version()
+    result['min_sdk_version'] = a.get_min_sdk_version()
+    return json.dumps(result)
 
 def main():
     if len(sys.argv) != 2:
@@ -77,11 +92,12 @@ def main():
             sys.exit(1)
         # force to delete directory by command (apktool d -f )
         apktoolAnalyze(f_name, f_md5)
-        androguardAnalyze(f_name, f_md5)
-        print '{"result":"Done."}'
+        result = androguardAnalyze(f_name, f_md5)
+        print '{"result":"Done.", "apk_info": %s}' % result
     except SystemExit:
         pass
     except:
+        sys.stderr.write("Unexpected error: %s\n" % sys.exc_info()[0])
         print '{"result":"Parse Error."}'
 
 if __name__ == "__main__":
