70c046c26f02b2cf0143cc8acf254ab0718e340b
Yi Huang <telgniw@me.com>
2013-11-03 01:01:24 +0000

    Change to $.ajax for upload and $.modal for showing result.

--- web/public/javascripts/index.js
+++ web/public/javascripts/index.js
@@ -4,37 +4,31 @@ $(function() {
     });
 
     $('.pure-file').change(function() {
-        var $file = $(this);
-        $('.pure-fake-file').val($file.val());
+        var path = $(this).val().split('\\');
+        $('.pure-fake-file').val(path.slice(-1)[0]);
     });
 
-    $('.pure-button').click(function() { 
-        var client = new XMLHttpRequest();
+    $('.pure-form').submit(function() { 
+        $('#modal-loading').show();
+        $('#modal-result').empty();
+        $('#myModal').modal('show');
 
-        function upload() {
-            var file = document.getElementById("apk");
-            /* Create a FormData instance */
-            var formData = new FormData();
-            /* Add the file */ 
-            formData.append("apk", file.files[0]);
+        $.ajax({
+            url: '/api/apk/analyze',
+            type: 'post',
+            success: function(data) {
+                $('#modal-loading').hide();
+                $('#modal-result')
+                    .append($('<pre></pre>').text(JSON.stringify(data, undefined, 2)))
+                    .show();
+                $('#myModal').modal('show');
+            },
+            data: new FormData($('.pure-form')[0]),
+            cache: false,
+            contentType: false,
+            processData: false
+        });
 
-            client.open("post", "/api/apk/analyze", true);
-            client.send(formData);  /* Send to server */ 
-        }
-
-        /* Check the response status */  
-        client.onreadystatechange = function() {
-            if (client.readyState == 4 && client.status == 200) {
-                if(client.statusText == "OK") {
-                    alert(client.response);
-                }
-            }
-        }
-        upload();
-        $('#loading').fadeIn();
-        $('#loading').css("display","block");
-        $('.pure-fake-file').prop('disabled','true');
-        $('.pure-button').prop('disabled','true')
         return false;
     });
 });
