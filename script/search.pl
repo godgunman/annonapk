b9633180b201690d135493fce068aa990a2307a9
Hung-Chi Su <atdog.cs96@gmail.com>
2013-11-03 03:11:15 +0000

    quich search all cached result

---
+++ script/search.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+#
+use strict;
+use Cwd 'abs_path';
+use File::Basename;
+use JSON::Parse 'json_file_to_perl';
+use JSON;
+use Data::Dumper;
+
+
+my $analyticsROOT = abs_path(dirname(abs_path($0))."/../apk/analytics/");
+
+sub main {
+    my $result = [];
+    opendir(my $D, $analyticsROOT) || die "Can't opedir: $!\n";
+    while (my $f = readdir($D)) {
+        next if $f =~ m/^\./;
+        my $resultFile = $analyticsROOT."/".$f."/result";
+        my $json = json_file_to_perl ($resultFile);
+        my $package = $json->{'package'};
+        my $link = "http://annonapk.com/apk/analytics/$f";
+        my %apk_info = (package => $package, link => $link);
+        push @$result, \%apk_info;
+    }
+    closedir($D);
+    print to_json($result);
+}
+
+main;
